a20b89ea4a892ddc44fb4f40fae000ab3a5f044f
Fabio Niephaus <code@fniephaus.com>
2020-07-31 16:45:10 +0200

    Introduce concrete return constant bytecode nodes

--- src/de.hpi.swa.trufflesqueak/src/de/hpi/swa/trufflesqueak/nodes/bytecodes/ReturnBytecodes.java
+++ src/de.hpi.swa.trufflesqueak/src/de/hpi/swa/trufflesqueak/nodes/bytecodes/ReturnBytecodes.java
@@ -13,10 +13,13 @@ import com.oracle.truffle.api.frame.VirtualFrame;
 
 import de.hpi.swa.trufflesqueak.exceptions.Returns.NonLocalReturn;
 import de.hpi.swa.trufflesqueak.exceptions.SqueakExceptions.SqueakException;
+import de.hpi.swa.trufflesqueak.model.BooleanObject;
 import de.hpi.swa.trufflesqueak.model.CompiledCodeObject;
 import de.hpi.swa.trufflesqueak.model.ContextObject;
 import de.hpi.swa.trufflesqueak.model.NilObject;
-import de.hpi.swa.trufflesqueak.nodes.bytecodes.ReturnBytecodesFactory.ReturnConstantNodeGen;
+import de.hpi.swa.trufflesqueak.nodes.bytecodes.ReturnBytecodesFactory.ReturnConstantFalseNodeGen;
+import de.hpi.swa.trufflesqueak.nodes.bytecodes.ReturnBytecodesFactory.ReturnConstantNilNodeGen;
+import de.hpi.swa.trufflesqueak.nodes.bytecodes.ReturnBytecodesFactory.ReturnConstantTrueNodeGen;
 import de.hpi.swa.trufflesqueak.nodes.bytecodes.ReturnBytecodesFactory.ReturnReceiverNodeGen;
 import de.hpi.swa.trufflesqueak.nodes.bytecodes.ReturnBytecodesFactory.ReturnTopFromBlockNodeGen;
 import de.hpi.swa.trufflesqueak.nodes.bytecodes.ReturnBytecodesFactory.ReturnTopFromMethodNodeGen;
@@ -89,32 +92,7 @@ public final class ReturnBytecodes {
         }
     }
 
-    public abstract static class ReturnConstantNode extends AbstractReturnWithSpecializationsNode {
-        public final Object constant;
-
-        protected ReturnConstantNode(final CompiledCodeObject code, final int index, final Object obj) {
-            super(code, index);
-            constant = obj;
-        }
-
-        public static ReturnConstantNode create(final CompiledCodeObject code, final int index, final Object value) {
-            return ReturnConstantNodeGen.create(code, index, value);
-        }
-
-        @Override
-        protected final Object getReturnValue(final VirtualFrame frame) {
-            return constant;
-        }
-
-        @Override
-        public final String toString() {
-            CompilerAsserts.neverPartOfCompilation();
-            return "return: " + constant.toString();
-        }
-    }
-
     public abstract static class ReturnReceiverNode extends AbstractReturnWithSpecializationsNode {
-
         protected ReturnReceiverNode(final CompiledCodeObject code, final int index) {
             super(code, index);
         }
@@ -132,6 +110,69 @@ public final class ReturnBytecodes {
         public final String toString() {
             CompilerAsserts.neverPartOfCompilation();
             return "returnSelf";
+        }
+    }
+
+    public abstract static class ReturnConstantTrueNode extends AbstractReturnWithSpecializationsNode {
+        protected ReturnConstantTrueNode(final CompiledCodeObject code, final int index) {
+            super(code, index);
+        }
+
+        public static ReturnConstantTrueNode create(final CompiledCodeObject code, final int index) {
+            return ReturnConstantTrueNodeGen.create(code, index);
+        }
+
+        @Override
+        protected final Object getReturnValue(final VirtualFrame frame) {
+            return BooleanObject.TRUE;
+        }
+
+        @Override
+        public final String toString() {
+            CompilerAsserts.neverPartOfCompilation();
+            return "return: true";
+        }
+    }
+
+    public abstract static class ReturnConstantFalseNode extends AbstractReturnWithSpecializationsNode {
+        protected ReturnConstantFalseNode(final CompiledCodeObject code, final int index) {
+            super(code, index);
+        }
+
+        public static ReturnConstantFalseNode create(final CompiledCodeObject code, final int index) {
+            return ReturnConstantFalseNodeGen.create(code, index);
+        }
+
+        @Override
+        protected final Object getReturnValue(final VirtualFrame frame) {
+            return BooleanObject.FALSE;
+        }
+
+        @Override
+        public final String toString() {
+            CompilerAsserts.neverPartOfCompilation();
+            return "return: false";
+        }
+    }
+
+    public abstract static class ReturnConstantNilNode extends AbstractReturnWithSpecializationsNode {
+        protected ReturnConstantNilNode(final CompiledCodeObject code, final int index) {
+            super(code, index);
+        }
+
+        public static ReturnConstantNilNode create(final CompiledCodeObject code, final int index) {
+            return ReturnConstantNilNodeGen.create(code, index);
+        }
+
+        @Override
+        protected final Object getReturnValue(final VirtualFrame frame) {
+            return NilObject.SINGLETON;
+        }
+
+        @Override
+        public final String toString() {
+            CompilerAsserts.neverPartOfCompilation();
+            return "return: nil";
         }
     }
 
